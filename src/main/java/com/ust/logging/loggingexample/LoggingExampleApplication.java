9b4d1eebac6862e8efe7b0a33bb85ebec485cd93
darco2018 <newsecond@outlook.com>
2021-11-16 14:16:03 +0100

    initial commit

---
+++ src/main/java/com/ust/logging/loggingexample/LoggingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ust.logging.loggingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingExampleApplication.class, args);
+	}
+
+}
